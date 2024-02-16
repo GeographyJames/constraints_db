52ec50c6e226d86a57dc756bae67ee771dcba321
James Campbell <james.e.campbell@outlook.com>
2024-02-16 11:44:52 +0000

    Added failing "goodbye" test.

--- src/db/main.py
+++ src/db/main.py
@@ -1,5 +1,8 @@
 def hello():
     return "hello"
 
+def goodbye():
+    return "goodbye"
+
 if __name__ == "__main__":
     print(f"{hello()} from database main.")
--- tests/db_tests/test_main.py
+++ tests/db_tests/test_main.py
@@ -3,3 +3,6 @@ from src.db import main
 class TestMain:
     def test_should_return_hello(self):
         assert main.hello() == "hello"
+
+    def test_should_return_goodbye(self):
+        assert main.goodbye() == "bye"
