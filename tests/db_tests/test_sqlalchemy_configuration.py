32da5ea0729f1cfecc6583b94291041bfe8b9232
SILVERSTONE\Owner <james.e.campbell@outlook.com>
2024-02-19 10:41:16 +0000

    Fixed failing sqlalchemy config test.

--- tests/db_tests/test_sqlalchemy_configuration.py
+++ tests/db_tests/test_sqlalchemy_configuration.py
@@ -10,7 +10,7 @@ db_credentials_ini = Path("db_credentials.ini")
 class TestCredentialsFromIni:
 
     def test_should_return_database_credentials(self) -> None:
-        assert isinstance(sqlalchemy_config.credentials_from_ini(Path("tests/db_tests/test_data/test_db_credentials_correct.ini")), sqlalchemy_config.DbCredentiails)
+        assert isinstance(sqlalchemy_config.credentials_from_ini(Path("tests/db_tests/test_data/test_db_credentials_correct.ini"), password="password"), sqlalchemy_config.DbCredentiails)
 
     def test_should_raise_credentials_error(self) -> None:
         with pytest.raises(CredentialsError):
