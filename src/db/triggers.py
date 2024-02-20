4f37bd26a06cffbbbb787ec453a7366f160e097e
SILVERSTONE\Owner <james.e.campbell@outlook.com>
2024-02-20 11:04:27 +0000

    None

--- src/db/triggers.py
+++ src/db/triggers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import DDL, Engine
+from sqlalchemy import DDL, Connection
 from .sqlalchemy_config import engine, credentials_from_ini
 import pathlib
 from typing import List
@@ -12,37 +12,39 @@ class Table:
 
 
 tables = [Table("public", "development_constraints"),
-          Table("public", "constriaint_categories")]
+          Table("public", "constraint_categories"),
+          Table("public", "priority_levels")]
 
 
-def create_triggers(engine: Engine, tables: List[Table]) -> None:
+def create_triggers(conn: Connection, tables: List[Table]) -> None:
     """Function to create database triggers to update databas tables with"""
     """current time and database user when updating a table row."""
-    with engine.connect() as conn:
-        for table in tables:
-            func = DDL(
-                f"CREATE OR REPLACE FUNCTION {
-                    table.schema}.{table.name}_update() "
-                "RETURNS trigger as $$ "
-                "BEGIN "
-                "NEW.last_update := current_timestamp; "
-                "NEW.last_update_by := current_user; "
-                "RETURN NEW; "
-                "END; "
-                "$$ LANGUAGE plpgsql;"
-            )
+    for table in tables:
+        func = DDL(
+            f"CREATE OR REPLACE FUNCTION "
+            f"{table.schema}.{table.name}_update() "
+            "RETURNS trigger as $$ "
+            "BEGIN "
+            "NEW.last_update := current_timestamp; "
+            "NEW.last_update_by := current_user; "
+            "RETURN NEW; "
+            "END; "
+            "$$ LANGUAGE plpgsql;"
+        )
 
-            trigger = DDL(
-                f"CREATE OR REPLACE TRIGGER "
-                f"{table}_update BEFORE UPDATE ON {table.schema}.{table.name} "
-                f"FOR EACH ROW EXECUTE FUNCTION {
-                    table.schema}.{table.name}_update();"
-            )
-            conn.execute(func)
-            conn.execute(trigger)
-            conn.commit()
+        trigger = DDL(
+            f"CREATE OR REPLACE TRIGGER "
+            f"{table.name}_update BEFORE UPDATE ON "
+            f"{table.schema}.{table.name} "
+            f"FOR EACH ROW EXECUTE FUNCTION "
+            f"{table.name}_update();"
+        )
+        conn.execute(func)
+        conn.execute(trigger)
+        conn.commit()
 
 
 if __name__ == "__main__":
-    create_triggers(engine(credentials_from_ini(
-        pathlib.Path("db_credentials.ini")), echo=True), tables)
+    with engine(credentials_from_ini(
+            pathlib.Path("db_credentials.ini")), echo=True).connect() as conn:
+        create_triggers(conn, tables)
