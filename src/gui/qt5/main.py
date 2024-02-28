236107efa5812ad55ba3780d488e7d1216b0a196
SILVERSTONE\Owner <james.e.campbell@outlook.com>
2024-02-28 07:32:45 +0000

    tried to fix flake8 error.

--- src/gui/qt5/main.py
+++ src/gui/qt5/main.py
@@ -1,10 +1,9 @@
-import sys
 from PyQt5.QtWidgets import QApplication
 from .dlgs.main_window import MainWindow
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(level = logging.INFO)
+    logging.basicConfig(level=logging.INFO)
     app = QApplication([])
     main_window = MainWindow()
     main_window.show()
