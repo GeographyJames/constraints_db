d93a3ec1a947c9c15a92d07f0dd54891b8e4bbcb
James Campbell <james.e.campbell@outlook.com>
2024-02-29 11:21:34 +0000

    fixed pytest

--- tests/db_tests/test_initialisation.py
+++ tests/db_tests/test_initialisation.py
@@ -3,9 +3,9 @@ from src.db.sqlalchemy_config import *
 from pathlib import Path
 import pytest
 
-engine = engine(credentials_from_ini(Path(f"db_credentials.ini")))
 
 
 @pytest.mark.db
 def test_initialise_constraints_tables():
+    engine = engine(credentials_from_ini(Path(f"db_credentials.ini")))
     set_constraint_layer_names(engine)
