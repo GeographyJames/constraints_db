a9e7a09509267f3167bcc2f0fae58c0bf62d2d60
James Campbell <james.e.campbell@outlook.com>
2024-03-01 15:54:20 +0000

    Added GDAL

--- tests/db_tests/test_sqlalchemy_configuration.py
+++ tests/db_tests/test_sqlalchemy_configuration.py
@@ -11,13 +11,13 @@ class TestCredentialsFromIni:
 
     def test_should_return_database_credentials(self) -> None:
         assert isinstance(sqlalchemy_config.credentials_from_ini(Path(
-            "tests/db_tests/test_data/test_db_credentials_correct.ini"),
+            "tests/test_data/test_db_credentials/test_db_credentials_correct.ini"),
             password="password"), sqlalchemy_config.DbCredentiails)
 
     def test_should_raise_credentials_error(self) -> None:
         with pytest.raises(CredentialsError):
             sqlalchemy_config.credentials_from_ini(
-                Path("tests/db_tests/test_data/test_db_credentials_error.ini"))
+                Path("tests/db_tests/test_data/test_db_credentials/test_db_credentials_error.ini"))
         with pytest.raises(
                 CredentialsError,
                 match="Unable to locate credentials .ini file"):
